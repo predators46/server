d7f5e17bd691a6222282df6f1a7c88e18dad397c
Predators <31931530+predators46@users.noreply.github.com>
2021-06-26 08:33:24 +0800

    Update m_string.h

--- include/m_string.h
+++ include/m_string.h
@@ -346,9 +346,7 @@ static inline const uchar *skip_trailing_space(const uchar *ptr,size_t len)
   if (len > 20)
   {
     const uchar *end_words= (const uchar *)(intptr)
-      (((ulonglong)(intptr)end) / SIZEOF_INT * SIZEOF_INT);
     const uchar *start_words= (const uchar *)(intptr)
-       ((((ulonglong)(intptr)ptr) + SIZEOF_INT - 1) / SIZEOF_INT * SIZEOF_INT);
 
     DBUG_ASSERT(((ulonglong)(intptr)ptr) >= SIZEOF_INT);
     if (end_words > ptr)
